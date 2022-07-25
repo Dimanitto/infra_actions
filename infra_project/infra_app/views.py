75a88901ef4fe891ad61e168b14fa1ff82fe4c3b
dmitry <d.selivanov@itcodegroup.ru>
2022-07-26 01:16:18 +0500

    changed main page2

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -1,8 +1,13 @@
 from django.http import HttpResponse
 
 
+html_code = """<img src="https://warfiles.ru/uploads/
+posts/2018-03/1521565606_1468661375_1.jpg"
+height="300px" />"""
+
+
 def index(request):
-    return HttpResponse('<img src="https://warfiles.ru/uploads/posts/2018-03/1521565606_1468661375_1.jpg" height="300px" />')
+    return HttpResponse(html_code)
 
 
 def second_page(request):
